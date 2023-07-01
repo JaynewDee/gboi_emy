9f4d805666f16335107f8eb4670730aa36ad794a
NuminousBlue <jdiehl2236@gmail.com>
2023-07-01 18:23:35 -0500

    instantiate Registers, setup getter-setters in registers implementation, init const flag bit indicators, implement from flagsregister for u8 and vice versa

---
+++ notes.md
@@ -0,0 +1,5 @@
+## Specs
+- CPU
+> Sharp LR35902
+> custom chip
+> similar to intel 8080 and zilog z80
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+struct Registers {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    h: u8,
+    l: u8
+}
+
+impl Registers {
+    fn get_bc(&self) -> u16 {
+        (self.b as u16) << 8 | self.c as u16
+    }
+
+    fn set_bc(&self, value: u16) {
+        self.b = ((value & 0xFF00) >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+}
